00bf8b72ab2726a6f8f6f2377c1fc4a65e425651
juanc1289 <juan.cartagena94@gmail.com>
2023-09-22 20:10:31 +0100

    primera version funcional del sms.py

--- sms.py
+++ sms.py
@@ -1,18 +1,34 @@
 import serial
 import time 
 
-phone = serial.Serial("/dev/ttyS0", baudrate=115200, timeout=1.0)
+phone = serial.Serial("/dev/ttyS0", baudrate=115200, timeout=2.0)
 
-message1='AT+CGNSPWR=1\r\n'
-messageutf1= message1.encode(encoding='utf-8', errors = 'strict')
+message='AT+CGNSPWR=1\r\n'
+message_utf= message.encode(encoding='utf-8', errors = 'strict')
 
-phone.write(messageutf1)
+phone.write(message_utf)
+result=phone.read(100)
+print(result)
+#print('message 1: ',message_utf)
+
+message='AT+CGNSINF\r\n'
+message_utf= message.encode(encoding='utf-8', errors = 'strict')
+
+phone.write(message_utf)
 result=phone.read(100)
 print(result)
 
-message2='AT+CGNSINF\r\n'
-messageutf2= message2.encode(encoding='utf-8', errors = 'strict')
 
-phone.write(messageutf2)
+message='AT+CMGF=1\r\n'
+message_utf= message.encode(encoding='utf-8', errors = 'strict')
+
+phone.write(message_utf)
+result=phone.read(100)
+print(result)
+
+message='AT+CMGR=1\r\n'
+message_utf= message.encode(encoding='utf-8', errors = 'strict')
+
+phone.write(message_utf)
 result=phone.read(100)
 print(result)
