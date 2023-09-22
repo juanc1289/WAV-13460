a6f1c413b2c7cf4ce324f5d4eaff6d168c843b94
juanc1289 <juan.cartagena94@gmail.com>
2023-09-22 20:21:32 +0100

    segunda version funcional del sms.py

--- sms.py
+++ sms.py
@@ -32,3 +32,19 @@ message_utf= message.encode(encoding='utf-8', errors = 'strict')
 phone.write(message_utf)
 result=phone.read(100)
 print(result)
+
+
+message='AT+CMGS="+32477560148"\r\n'
+message_utf= message.encode(encoding='utf-8', errors = 'strict')
+
+phone.write(message_utf)
+result=phone.read(100)
+print(result)
+
+message='hello world \032'
+message_utf= message.encode(encoding='utf-8', errors = 'strict')
+
+phone.write(message_utf)
+result=phone.read(100)
+print(result)
+
